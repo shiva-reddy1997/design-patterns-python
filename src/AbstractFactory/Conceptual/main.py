4d7a313ae3233edf0598e26f885aabfb07e0f5df
Nareddy Shiva Prasad Reddy <nspr12345@gmail.com>
2022-11-20 14:02:17 +0000

    removed the russian commit

--- src/AbstractFactory/Conceptual/main.py
+++ src/AbstractFactory/Conceptual/main.py
@@ -4,10 +4,6 @@ EN: Abstract Factory Design Pattern
 Intent: Lets you produce families of related objects without specifying their
 concrete classes.
 
-RU: Паттерн Абстрактная Фабрика
-
-Назначение: Предоставляет интерфейс для создания семейств связанных или
-зависимых объектов без привязки к их конкретным классам.
 """
 
 
@@ -24,12 +20,6 @@ class AbstractFactory(ABC):
     variants, but the products of one variant are incompatible with products of
     another.
 
-    RU: Интерфейс Абстрактной Фабрики объявляет набор методов, которые
-    возвращают различные абстрактные продукты. Эти продукты называются
-    семейством и связаны темой или концепцией высокого уровня. Продукты одного
-    семейства обычно могут взаимодействовать между собой. Семейство продуктов
-    может иметь несколько вариаций, но продукты одной вариации несовместимы с
-    продуктами другой.
     """
     @abstractmethod
     def create_product_a(self) -> AbstractProductA:
@@ -47,10 +37,6 @@ class ConcreteFactory1(AbstractFactory):
     that signatures of the Concrete Factory's methods return an abstract
     product, while inside the method a concrete product is instantiated.
 
-    RU: Конкретная Фабрика производит семейство продуктов одной вариации.
-    Фабрика гарантирует совместимость полученных продуктов. Обратите внимание,
-    что сигнатуры методов Конкретной Фабрики возвращают абстрактный продукт, в
-    то время как внутри метода создается экземпляр конкретного продукта.
     """
 
     def create_product_a(self) -> AbstractProductA:
@@ -64,7 +50,6 @@ class ConcreteFactory2(AbstractFactory):
     """
     EN: Each Concrete Factory has a corresponding product variant.
 
-    RU: Каждая Конкретная Фабрика имеет соответствующую вариацию продукта.
     """
 
     def create_product_a(self) -> AbstractProductA:
@@ -79,8 +64,6 @@ class AbstractProductA(ABC):
     EN: Each distinct product of a product family should have a base interface.
     All variants of the product must implement this interface.
 
-    RU: Каждый отдельный продукт семейства продуктов должен иметь базовый
-    интерфейс. Все вариации продукта должны реализовывать этот интерфейс.
     """
 
     @abstractmethod
@@ -91,7 +74,6 @@ class AbstractProductA(ABC):
 """
 EN: Concrete Products are created by corresponding Concrete Factories.
 
-RU: Конкретные продукты создаются соответствующими Конкретными Фабриками.
 """
 
 
@@ -111,16 +93,12 @@ class AbstractProductB(ABC):
     interact with each other, but proper interaction is possible only between
     products of the same concrete variant.
 
-    RU: Базовый интерфейс другого продукта. Все продукты могут взаимодействовать
-    друг с другом, но правильное взаимодействие возможно только между продуктами
-    одной и той же конкретной вариации.
     """
     @abstractmethod
     def useful_function_b(self) -> None:
         """
         EN: Product B is able to do its own thing...
 
-        RU: Продукт B способен работать самостоятельно...
         """
         pass
 
@@ -132,10 +110,6 @@ class AbstractProductB(ABC):
         The Abstract Factory makes sure that all products it creates are of the
         same variant and thus, compatible.
 
-        RU: ...а также взаимодействовать с Продуктами A той же вариации.
-
-        Абстрактная Фабрика гарантирует, что все продукты, которые она создает,
-        имеют одинаковую вариацию и, следовательно, совместимы.
         """
         pass
 
@@ -143,7 +117,6 @@ class AbstractProductB(ABC):
 """
 EN: Concrete Products are created by corresponding Concrete Factories.
 
-RU: Конкретные Продукты создаются соответствующими Конкретными Фабриками.
 """
 
 
@@ -156,8 +129,6 @@ class ConcreteProductB1(AbstractProductB):
     variant, Product A1. Nevertheless, it accepts any instance of
     AbstractProductA as an argument.
 
-    RU: Продукт B1 может корректно работать только с Продуктом A1. Тем не менее,
-    он принимает любой экземпляр Абстрактного Продукта А в качестве аргумента.
     """
 
     def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
@@ -175,9 +146,6 @@ class ConcreteProductB2(AbstractProductB):
         variant, Product A2. Nevertheless, it accepts any instance of
         AbstractProductA as an argument.
 
-        RU: Продукт B2 может корректно работать только с Продуктом A2. Тем не
-        менее, он принимает любой экземпляр Абстрактного Продукта А в качестве
-        аргумента.
         """
         result = collaborator.useful_function_a()
         return f"The result of the B2 collaborating with the ({result})"
@@ -189,10 +157,6 @@ def client_code(factory: AbstractFactory) -> None:
     types: AbstractFactory and AbstractProduct. This lets you pass any factory
     or product subclass to the client code without breaking it.
 
-    RU: Клиентский код работает с фабриками и продуктами только через
-    абстрактные типы: Абстрактная Фабрика и Абстрактный Продукт. Это позволяет
-    передавать любой подкласс фабрики или продукта клиентскому коду, не нарушая
-    его.
     """
     product_a = factory.create_product_a()
     product_b = factory.create_product_b()
@@ -205,7 +169,6 @@ if __name__ == "__main__":
     """
     EN: The client code can work with any concrete factory class.
 
-    RU: Клиентский код может работать с любым конкретным классом фабрики.
     """
     print("Client: Testing client code with the first factory type:")
     client_code(ConcreteFactory1())
